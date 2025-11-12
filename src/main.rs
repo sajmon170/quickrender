64b56b28ae68644c0de638711ed2615f002532a7
sajmon <szymon17099@gmail.com>
2025-11-12 11:37:36 +0100

    Set winit control flow to wait

--- src/main.rs
+++ src/main.rs
@@ -160,6 +160,6 @@ impl App {
 
 fn main() {
     let event_loop = EventLoop::new().unwrap();
-    event_loop.set_control_flow(ControlFlow::Poll);
+    event_loop.set_control_flow(ControlFlow::Wait);
     event_loop.run_app(&mut App::default()).unwrap();
 }
